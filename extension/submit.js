247730d0c7484d3273fd14c5cf9975c375859e54
Marta Spahija <marta.spahija@gmail.com>
2021-03-11 10:31:41 +0100

    fix url in manifest.json and submit.js and change logo

--- extension/submit.js
+++ extension/submit.js
@@ -17,7 +17,7 @@ function submitData() {
                 item_url: item_url
             }
 
-            fetch("//http://158.101.23.186:3000/", {
+            fetch("http://158.101.23.186:3000/", {
                 method: "POST",
                 body: JSON.stringify(data),
                 headers: {"Content-type": "application/json; charset=UTF-8"},
